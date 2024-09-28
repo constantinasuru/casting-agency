cfc6738378b3f22e2bcfd24fcee9536be3e049a3
Cristina.sr29@yahoo.com <constantina.suru@0124EITR0002.emag.local>
2024-09-28 18:57:43 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,50 @@
+import ast
+import os
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+database_path = os.environ['DATABASE_URL']
+if database_path.startswith("postgres://"):
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+# Association table for the many-to-many relationship
+actor_movie = db.Table('actor_movie',
+    db.Column('actor_id', db.Integer, db.ForeignKey('actor.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True)
+)
+
+class Actor(db.Model):
+    __tablename__ = 'actor'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(), nullable=False)
+
+    movies = db.relationship('Movie', secondary=actor_movie, lazy='subquery', backref=db.backref('actors', lazy=True))
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(), nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://postgres@localhost:5432/casting_agency"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
